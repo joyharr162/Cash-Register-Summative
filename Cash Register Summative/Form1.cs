974446272f3f6ffe29b755181f52c3b8e6ad5575
joyharr162 <joyharr162@ed.amdsb.ca>
2016-10-13 21:20:09 -0400

    Update Form1.cs

--- Cash Register Summative/Form1.cs
+++ Cash Register Summative/Form1.cs
@@ -1,4 +1,83 @@
-﻿using System;
+///Created by Joy Harris
+///October 14 2016
+///For the purpose of simulating a store cash register
+
+//try math operations using only numbers, change the type of declaration on the int's
+using S﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace Cash_Register_Summative
+{
+    public partial class Form1 : Form
+    {
+        //declare variables and constants
+        int ccmQuantity = 0;
+        int cmQuantity = 0;
+        int bmQuantity = 0;
+        double change = 0;
+        double taxPrice = 0;
+        double carrotPrice = 0;
+        double chocolatePrice = 0;
+        const double CARROTCOST = 3.25;
+        const double CHOCCHIPCOST = 3.5;
+        const double BLUEBERRYCOST = 2.9;
+        const double TAX = .13;
+        const int ORDERNUMBER = 1;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            //assign number of each muffin to the appropriate variable
+            try
+            {
+              ccmQuantity = Convert.ToInt16(chocChipMuffinBox);
+              cmQuantity = Convert.ToInt16(carrotMuffinBox);
+              bmQuantity = Convert.ToInt16(blueberryMuffinBox);
+            }
+            catch
+            {
+                //if letters are used, tell user to input integers only
+                tryCatchLabel.Text = "Please input integrs only";
+            }
+        }
+
+        private void subtotalButton_Click(object sender, EventArgs e)
+        {
+            //calculate the total before tax 
+            carrotPrice = cmQuantity * CARROTCOST;
+            chocolatePrice = ccmQuantity * CHOCCHIPCOST;
+            blueberryPrice = bmQuantity * BLUEBERRYCOST;
+            subtotal = blueberryPrice + carrotPrice + chocolatePrice;
+            subtotalLabel.Text = Convert.ToString(subtotal);
+        }
+
+        private void taxButton_Click(object sender, EventArgs e)
+        {
+            //calculate tax
+            taxLabel.Text = Convert.ToString(TAX * subtotal);
+        }
+
+        private void grandTotalButton_Click(object sender, EventArgs e)
+        {
+            //calculate the total after tax
+            taxPrice = Convert.ToInt16(taxLabel.Text);
+            grandTotalLabel.Text = Convert.ToString(subtotal + taxPrice);
+        }
+
+        private void receiptButton_Click(object sender, EventArgs e)
+        {
+            //make pens, fonts, etc.
+            Font drawFont = new Font("Consolas", 8, FontStyle.Bold);
+            Pen receiptPen = new Pen(Color.LightPink, 5);ystem;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
